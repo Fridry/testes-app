d57260291cca311463cafca88d54038bc59862dc
Eduardo <fridry@DESKTOP-4KHOBL6.localdomain>
2022-11-07 20:22:48 -0300

    Finish Prisma Configuration

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+import { products } from '../products';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for (const product of products) {
+    await prisma.product.upsert({
+      where: { sku: product.sku },
+      update: {},
+      create: product,
+    });
+  }
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
